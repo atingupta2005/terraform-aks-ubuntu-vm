dbbb6bbd883bb81658399706ef53ec3f5bb4aeb2
Atin Gupta <atingupta@email.com>
2023-06-25 18:09:41 +0530

    -

--- via azure-cli/vm/create-vms.sh
+++ via azure-cli/vm/create-vms.sh
@@ -1,10 +1,10 @@
-subscription_id="6378784e-0a8a-4197-a15d-afc70adb2952"
+subscription_id="acbda50e-8228-408d-90dd-1e3fc7b0c1e7"
 
 az account set --subscription $subscription_id
 
 mylocations=("eastus" "eastus2"  "japaneast" "westus" "northeurope" "uksouth")
 
-for i in $(seq 0 5); do
+for i in $(seq 1 6); do
     mylocation=${mylocations[$i]}
 	VM_RG="rg-vm-$i"
 
@@ -13,13 +13,13 @@ for i in $(seq 0 5); do
 	#azure network vnet create samplevnet -l $mylocation
 done
 
-for i in $(seq 1 5); do
+for i in $(seq 1 3); do
     mylocation=${mylocations[$i]}
 	VM_RG="rg-vm-$i"
 	
-	for j in $(seq 1 6); do
+	for j in $(seq 1 10); do
 		VM_NAME="vm-$i-$j"
 
-az vm create -n $VM_NAME -g $VM_RG --image Ubuntu2204 --location $mylocation --size Standard_B1S   --admin-username tuser --admin-password "Azure@123456" --ssh-key-values terraform-azure.pub --accept-term --authentication-type all --custom-data app1-cloud-init.txt --subscription "$subscription_id" --public-ip-sku Basic --public-ip-address-allocation dynamic --public-ip-address-dns-name "lnx-hpqzdp-$i-$j"
+		echo az vm create -n $VM_NAME -g $VM_RG --image Ubuntu1804 --location $mylocation --size Standard_B1S   --admin-username tuser --admin-password "Azure@123456" --ssh-key-values terraform-azure.pub --accept-term --authentication-type all --custom-data app1-cloud-init.txt --subscription "$subscription_id" --public-ip-sku Basic --public-ip-address-allocation dynamic --public-ip-address-dns-name "lnx-hpqzdp-$i-$j"
 	done
 done
