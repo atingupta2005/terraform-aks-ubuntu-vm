b9f6112cea6602f87077be847d110979170bf786
Atin Gupta <atingupta@email.com>
2023-06-25 13:54:39 +0530

    -

--- via-terraform/vms/c5-outputs.tf
+++ via-terraform/vms/c5-outputs.tf
@@ -1,24 +1,3 @@
-# 1. Output Values - Resource Group
-output "resource_group_id" {
-  description = "Resource Group ID"
-  # Atrribute Reference
-  value = azurerm_resource_group.myrg[*].id 
-}
-output "resource_group_name" {
-  description = "Resource Group name"
-  # Argument Reference
-  value = azurerm_resource_group.myrg[*].name  
-}
-
-# 2. Output Values - Virtual Network
-output "virtual_network_name" {
-  description = "Virutal Network Name"
-  #value = azurerm_virtual_network.myvnet.name 
-  value = azurerm_virtual_network.myvnet[*].name 
-  #sensitive = true
-}
-
-# 3. Output Values - DNS Name
 output "dns_name" {
-  value = azurerm_public_ip.mypublicip[*].domain_name_label 
+  value = azurerm_public_ip.mypublicip[*].fqdn 
 }
