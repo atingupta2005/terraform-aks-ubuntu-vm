0efce8b3a6df13802b2bca40ec83a894cd7be6ee
Atin Gupta <atingupta@email.com>
2023-06-25 14:24:37 +0530

    -

--- ansible-configure-ubuntu/login-azure-cli.sh
+++ ansible-configure-ubuntu/login-azure-cli.sh
@@ -1,6 +1,5 @@
 az login -u asreet-tech@atttrainings.com -p Azure@123456
 az account set --subscription 6378784e-0a8a-4197-a15d-afc70adb2952
 az aks get-credentials --resource-group rg-aks-0 --name aks-0
-sudo snap install kubectl --classic
 kubectl get nodes -o wide
 kubectl get pods -o wide -A
